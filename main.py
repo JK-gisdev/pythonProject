902432329a9587c87c0db8525be2b0f7e38e34a3
JK <kmiecikjakub@gmail.com>
2022-04-17 10:55:25 +0200

    Dodanie nowej linijki

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi('How are you')
+    print_hi('What time is it')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
